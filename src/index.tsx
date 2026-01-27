c387940e31e167c0ac2fa62854c88145a0d38023
nhatbantrongtoi98-code <nhatbantrongtoi98@gmail.com>
2026-01-27 12:49:20 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App'; // Kết nối trực tiếp với file App.tsx trong cùng thư mục src
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
